59873ae39040990dc3070f3041eb9dbb254ebcc9
Nick Gauthier <ngauthier@gmail.com>
2012-08-20 15:34:21 -0400

    more complex delete

--- src/blog/test/integration/posts/delete_test.rb
+++ src/blog/test/integration/posts/delete_test.rb
@@ -2,9 +2,17 @@ require 'test_helper'
 
 class PostsDeleteTest < ActionDispatch::IntegrationTest
   test "delete a post" do
-    @post = Dom::Post.create
-    assert_equal 1, Dom::Post.count
+    Dom::Post.create(
+      title: "I am awesome"
+    )
+    @post = Dom::Post.create(
+      title: "I am lame"
+    )
+    assert_equal 2, Dom::Post.count
     @post.delete
-    assert_equal 0, Dom::Post.count
+    assert_equal 1, Dom::Post.count
+    assert_nil Dom::Post.find_by_title "I am lame"
+    refute_nil Dom::Post.find_by_title "I am awesome"
+    assert_equal ["I am awesome"], Dom::Post.map(&:title)
   end
 end
